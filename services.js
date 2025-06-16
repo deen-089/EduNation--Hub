5bef4dfa6e42be640c7160122dc7f987fd0a831c
Lameed Shamsondeen <shamsondeen256@gmail.com>
2025-06-16 12:50:39 +0100

    chang

--- services.js
+++ services.js
@@ -3,108 +3,126 @@ const products = [
      id: 1,
      title: "Mathematics Textbook", 
      price: 7500, 
+      discount: 10000,
      image: "math.webp" 
     },
   { 
     id: 2, 
     title: "Science Workbook", 
     price: 4000, 
+    discount: 5000,
     image: "science.webp" 
   },
   { 
     id: 3, 
-    title: "Educational App Subscription", 
+    title: " App Subscription", 
     price: 10000, 
+    discount: 12000,
     image: "suscription.webp" 
   },
   { 
     id: 4, 
     title: "School Supplies Kit", 
     price: 11500,
+    discount: 0,
      image: "supplies.webp" 
     },
   { 
     id: 5, 
     title: "School Bag", 
     price: 12000, 
+    discount: 15000,
     image: "bag.webp" 
   },
   { 
     id: 6, 
     title: "Stationary Mock Up", 
     price: 15000, 
+    discount: 20000,
     image: "station.webp" 
   },
   { 
     id: 7, 
     title: "Water Bottle", 
     price: 5000, 
+    discount: 0,
     image: "water.webp" 
   },
   { 
     id: 8, 
     title: "School Merchandise", 
     price: 25000, 
+    discount: 30000,
     image: "march.webp" 
   },
   { 
     id: 9, 
     title: "Pen", 
-    price: 2550, image: 
-    "pen.webp" 
+    price: 2550,
+    discount: 3000,
+    image:"pen.webp" 
   },
   { 
     id: 10, 
     title: "Mathematical Set", 
     price: 8550, 
+    discount: 0,
     image: "mathset.webp" 
   },
   { 
     id: 11, 
     title: "Food Flask", 
     price: 6000, 
+    discount: 8000,
     image: "flasks.webp" 
   },
   { 
     id: 12, 
     title: "Lunch Box", 
     price: 17000, 
+    discount: 20000,
     image: "lunch box.webp" 
   },
   { 
     id: 13, 
     title: "Marker", 
     price: 3000, 
+    discount: 5000,
     image: "marker.webp" 
   },
    { 
     id: 14, 
     title: "Educational Games", 
     price: 23300, 
+    discount: 30000,
     image: "game.webp" 
   },
    { 
     id: 15, 
-    title: " Printed Wall Charts and posters", 
+    title: " Printed Wall Charts", 
     price: 10500, 
+    discount: 15000,
     image: "wall.webp" 
   },
    { 
     id: 16, 
     title: " Notebook", 
     price: 1700, 
+    discount: 2500,
     image: "note.webp" 
   },
   { 
     id: 17, 
     title: "English Textbook", 
     price: 14000, 
+    discount: 9000,
     image: "eng.webp" 
   },
    { 
     id: 18, 
     title: "Dictionary", 
     price: 16000, 
+    discount:19500,
     image: "dictionary.webp" 
   }
 ];
@@ -121,7 +139,8 @@ function renderProducts(productList) {
     productCard.innerHTML = `
       <img src="${product.image}" alt="${product.title}" />
       <h3>${product.title}</h3>
-      <p style="font-weight:bolder;">&#8358;${product.price}</p>
+      <p style="font-weight:bolder; font-size:20px;">&#8358;${product.price}</p>
+      <p style="text-decoration: line-through; color:red; font-weight:bold; ">&#8358;${product.discount}</p>
       <button data-id="${product.id}">Add to Cart</button>
     `;
     productsContainer.appendChild(productCard);
